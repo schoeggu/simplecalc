3f900c274ebb33b099fa3858e3c8e0d5d124ddb2
Joel Sutter <joel-sutter@bluewin.ch>
2011-01-28 11:06:37 +0100

    also on os x

--- src/apps/util.h
+++ src/apps/util.h
@@ -1,29 +1,42 @@
+#include <unistd.h>
+#include <string>
+
 #ifdef WIN32
 	static const char PATH_DELIM = '\\';
 #endif
 
-#ifdef __linux__
+#if defined(__linux) || defined(__APPLE__)
+	#include <sys/types.h>
+	#include <pwd.h>
 	static const char PATH_DELIM = '/';
 #endif
-
-#include <unistd.h>
-#include <string>
-
 
 std::string getHomeDirectory()
 {
 	std::string dir;
-	char* env;
+	char* home;
+
 	#ifdef WIN32
-		if (env = getenv("USERPROFILE")) dir = env;
-		else if (env = getenv("HOMEDRIVE")) {
-			dir = env;
-			dir += getenv("HOMEPATH");
+		char* drive;
+		home = getenv("USERPROFILE");
+		if (home) dir = home;
+		else {
+			drive = getenv("HOMEDRIVE");
+			home = getenv("HOMEPATH");
+			if (drive && home) {
+				dir = drive;
+				dir += home;
+			}
 		}
 	#endif
 
-	#ifdef __linux__
-		if (env = getenv("HOME")) dir = env;
+	#if defined(__linux) || defined(__APPLE__)
+		home = getenv("HOME");
+		if (home) dir = home;
+		else {
+			passwd* pwd = getpwuid(getuid());
+			if (pwd) dir = pwd->pw_dir;
+		}
 	#endif
 	return dir;
 }
